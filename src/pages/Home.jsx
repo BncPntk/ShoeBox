5b6f1b351229ad634abbcbbe1efe02c91ad90671
Bence Pentek <pntkbence@gmail.com>
2023-12-22 13:22:40 +0100

    feat: slider images

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,13 @@ import PageNav from '../components/PageNav';
 import { getPageTitle } from '../App';
 import { Helmet } from 'react-helmet';
 
+const imageContext = import.meta.glob('/public/slider/*.{png,jpg,jpeg,svg}');
+
+const slides = Object.entries(imageContext).map(([path]) => ({
+  id: path,
+  path: path,
+}));
+
 function Home() {
   return (
     <div>
@@ -9,7 +16,16 @@ function Home() {
         <title>{getPageTitle()}</title>
       </Helmet>
       <PageNav />
-      <h1 className='text-4xl text-center'>ShoeBox</h1>
+      <div className='max-w-[1152px] h-[400px] w-full py-16 px-4 relative'>
+        <div
+          style={{ backgroundImage: `url(${slides[0].path})` }}
+          className='w-full h-full duration-700 bg-center bg-cover rounded-2xl'
+        >
+          {/* {slides.map((slide) => (
+            <img key={slide.id} src={slide.path} alt={`Slide of ${slide.id}`} />
+          ))} */}
+        </div>
+      </div>
     </div>
   );
 }
