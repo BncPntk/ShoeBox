523c8027e198ad886673cf56e326659ee8e82462
Bence Pentek <pntkbence@gmail.com>
2024-01-21 22:30:00 +0100

    feat: add loading and error comps

--- frontend/src/components/ErrorRow.jsx
+++ frontend/src/components/ErrorRow.jsx
@@ -1,5 +1,15 @@
 function ErrorRow() {
-  return <div>Error...</div>;
+  return (
+    <section className='px-4 py-16 '>
+      <div className='max-w-[1152px] mx-auto'>
+        <div className='flex justify-center'>
+          <div className='px-8 py-4 text-2xl text-center text-white bg-black'>
+            <p>Oops! Something went wrong while fetching data from the server.</p>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default ErrorRow;
--- frontend/src/components/Loader.jsx
+++ frontend/src/components/Loader.jsx
@@ -1,5 +1,27 @@
+import { ThreeDots } from 'react-loader-spinner';
+
 function Loader() {
-  return <div>Loading..</div>;
+  return (
+    <section className='px-4 py-16 '>
+      <div className='max-w-[1152px] mx-auto'>
+        <div className='flex flex-col px-4 mx-auto w-[160px]'>
+          <div className='mx-auto'>
+            <ThreeDots
+              visible={true}
+              height='60'
+              width='120'
+              color='#322821'
+              radius='9'
+              ariaLabel='three-dots-loading'
+              wrapperStyle={{}}
+              wrapperClass=''
+            />
+          </div>
+          <p className='text-lg font-medium text-center opacity-65'>Loading...</p>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Loader;
